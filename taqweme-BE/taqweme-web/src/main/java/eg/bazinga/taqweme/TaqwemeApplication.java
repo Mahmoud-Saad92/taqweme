f3eebcecc7a38490dc02851478ae7ff749fd078a
mahmoud.saad92 <ma7moud.sa3d92@gmail.com>
2020-06-12 21:16:06 +0200

    initial commit on back end -create project-

---
+++ taqweme-BE/taqweme-web/src/main/java/eg/bazinga/taqweme/TaqwemeApplication.java
@@ -0,0 +1,13 @@
+package eg.bazinga.taqweme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaqwemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaqwemeApplication.class, args);
+    }
+
+}
